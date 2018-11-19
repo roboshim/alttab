4ead9008c610819acc0d89344a3f4903e4daee97
Robert Wolf <r.wolf.cz@gmail.com>
2018-11-20 00:08:32 +0100

    define shortcuts for g.option_prevCode and g.option_nextCode

--- src/alttab.c
+++ src/alttab.c
@@ -243,6 +243,8 @@ int use_args_and_xrm(int *argc, char **argv)
 #define  KC  g.option_keyCode
 #define  GMM  g.option_modMask
 #define  GBM  g.option_backMask
+#define  PKC  g.option_prevCode
+#define  NKC  g.option_nextCode
 
     ksi = ksym_option_to_keycode(&db, XRMAPPNAME, "modifier", &errmsg);
     if (ksi == -1)

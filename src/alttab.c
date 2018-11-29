ca1faeaea687eac5617eb5db19cc31566cde0251
Robert Wolf <r.wolf.cz@gmail.com>
2018-11-29 10:58:16 +0100

    show UI in backward mode if backward modifier or revmodifier has been pressed

--- src/alttab.c
+++ src/alttab.c
@@ -571,7 +571,7 @@ int main(int argc, char **argv)
             }                   // safety redundance
             activeState = ev.xkey.state;
             if (!g.uiShowHasRun) {
-                uiShow((ev.xkey.state & g.option_backMask));
+                uiShow((ev.xkey.state & ( g.option_backMask | g.option_rModMask )));
             } else {
                 if ((ev.xkey.state & g.option_backMask) && (ev.xkey.keycode == g.option_keyCode)) {
                     uiPrevWindow();
